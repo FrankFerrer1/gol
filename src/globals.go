3936b111dd884f3a96eee4c6c266609989d63629
David Attid <david.attid@gmail.com>
2023-10-24 19:29:04 -0600

    updated docker compose file to include ngp-server using dockerfile

--- src/globals.go
+++ src/globals.go
@@ -1,9 +1,9 @@
 package main
 
 // TODO: INSERT PROPER HOSTNAME, IDEALLY ONE PUBLICALLY EXPOSED
-const POSTGRES_HOST = "tbd"
+const POSTGRES_HOST = "localhost"
 const POSTGRES_PORT = "5432"
 
-const POSTGRES_USER = "user"
-const POSTGRES_PASSWORD = "password123"
+const POSTGRES_USER = "postgres"
+const POSTGRES_PASSWORD = "postgres"
 const POSTGRES_DATABASE_NAME = "ngp-dev"
